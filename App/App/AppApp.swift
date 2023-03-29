85b25f4ad402ae223efd1a28266afeb922d854c4
layla <cisneroslay@hotmail.com>
2023-03-29 11:11:29 -0300

    A ASODKQWE

---
+++ App/App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Layla Cisneros on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStoryBoard/AppStoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoryBoard
+//
+//  Created by Layla Cisneros on 28/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
